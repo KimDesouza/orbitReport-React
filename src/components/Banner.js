6b3b8a15055ef0c50f89be9585bd1750f2a7fc31
KimDesouza <kimid2000@gmail.com>
2023-08-15 20:02:25 -0500

    First commit.

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+function Banner(){
+    return(
+        <div>
+            <section>
+            <header>Orbit Report</header>
+            </section>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    );   
+}
+
+export default Banner;
